c3519dbde6c282a327b439842818b6b4310e4282
est-personal <esteen.personalwork@gmail.com>
2026-07-28 14:22:08 +0800

    Add Test Scripts for QA Playground - Dropdowns

--- config/QaPlaygroundUrls.js
+++ config/QaPlaygroundUrls.js
@@ -2,6 +2,8 @@
 const QaPlaygroundUrls = {
     buttonsPage:
         'https://qaplayground.com/practice/buttons',
+    dropdownsPage:
+        'https://qaplayground.com/practice/dropdowns',
     formsPage:
         'https://qaplayground.com/practice/forms',
     homePage:
---
+++ locators/DropdownsLocators.js
@@ -0,0 +1,45 @@
+// Arrange Alphabetically
+// Locators for QA Playground - Dropdowns Page
+const DropdownsLocators = {
+    customPriorityDropdown:
+        '[data-testid="priority-dropdown-trigger"]',
+    customPriorityResult:
+        '[data-testid="result-s05"]',
+    customPrioritySection:
+        '[data-testid="scenario-custom-priority"]',
+    multiSelectHeroesMultiSelect: 
+        '[data-testid="hero-select"]',
+    multiSelectHeroesResult: 
+        '[data-testid="result-s04"]',
+    multiSelectHeroesSection: 
+        '[data-testid="scenario-multi-select-heroes"]',
+    searchCityDropdown: 
+        '[id="citySearch"]',
+    searchCityResult: 
+        '[data-testid="result-s06"]',
+    searchCitySection: 
+        '[data-testid="scenario-search-city"]',
+    selectCountryDropdown: 
+        '[data-testid="country-select"]',
+    selectCountryResult: 
+        '[data-testid="result-s02"]',
+    selectCountrySection: 
+        '[data-testid="scenario-select-country"]',
+    selectFruitDropdown: 
+        '[data-testid="fruit-select"]',
+    selectFruitResult: 
+        '[data-testid="result-s01"]',
+    selectFruitSection: 
+        '[data-testid="scenario-select-fruit"]',
+    selectLanguageDropdown: 
+        '[data-testid="language-select"]',
+    selectLanguageResult: 
+        '[data-testid="result-s03"]',
+    selectLanguageSection: 
+        '[data-testid="scenario-select-language"]',
+    selectLastButton: 
+        '[id="selectLastLanguageBtn"]',
+    
+};
+
+module.exports = { DropdownsLocators };
